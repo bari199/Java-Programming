76992c338305ccf685754a752e735682e29e9ede
Barik <bari1999smab@gmail.com>
2024-07-09 08:42:02 +0100

    AreaofCone programme added in Area_and_Folder file

---
+++ AreaofCone.java
@@ -0,0 +1,15 @@
+/*Java Program  find surface Area of cone*/
+import java.util.Scanner;
+class AreaofCone{
+	public static void main(String[] args) {
+		Scanner mc=new Scanner(System.in);
+		System.out.println("Enter the Radius of ConeS: ");
+		double r=mc.nextDouble();
+		System.out.println("enter the height of Cons: ");
+		double h=mc.nextDouble();
+
+		double volume = (22*r*r*h)/(3*7);
+		System.out.println("volume of cons is : "+volume);
+
+	}
+}
